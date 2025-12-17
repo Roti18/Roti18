283a5d07dcac5f0a454afc7cce40eb6e9968441f
Roti18 <akuronif@gmail.com>
2025-12-17 18:00:23 +0700

    add: hash password bcryptjs

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -2,7 +2,8 @@ import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const user = sqliteTable('user', {
 	id: text('id').primaryKey(),
-	email: text('email').unique(),
+	email: text('email').notNull().unique(),
+	passwordHash: text('password_hash').notNull(),
 	name: text('name'),
 	image: text('image')
 });
