7c002596caad56c671b86db4c7d47b7df99fb41a
Roti18 <akuronif@gmail.com>
2025-12-17 16:41:19 +0700

    edit: table hero and profile

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,11 @@
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
-export const user = sqliteTable('user', { id: text('id').primaryKey(), age: integer('age') });
+export const user = sqliteTable('user', {
+	id: text('id').primaryKey(),
+	email: text('email').unique(),
+	name: text('name'),
+	image: text('image')
+});
 
 export const session = sqliteTable('session', {
 	id: text('id').primaryKey(),
@@ -10,6 +15,31 @@ export const session = sqliteTable('session', {
 	expiresAt: integer('expires_at', { mode: 'timestamp' }).notNull()
 });
 
-export type Session = typeof session.$inferSelect;
+export const profile = sqliteTable('profile', {
+	id: integer('id').primaryKey(),
+	title: text('title').notNull(),
+	leftText: text('left_text').notNull(),
+	rightText: text('right_text').notNull()
+});
+
+export const hero = sqliteTable('hero', {
+	id: integer('id').primaryKey(),
+	name: text('name').notNull(), // M. ZAMRONI FAHREZA
+	role: text('role').notNull(), // DESIGNER & CREATIVE DIRECTOR
+	tagline: text('tagline').notNull() // Crafting narratives...
+});
+
+export const projects = sqliteTable('projects', {
+	id: integer('id').primaryKey(),
+	title: text('title').notNull(),
+	subtitle: text('subtitle').notNull(),
+	description: text('description').notNull(),
+	image: text('image').notNull(),
+	url: text('url').notNull()
+});
 
 export type User = typeof user.$inferSelect;
+export type Session = typeof session.$inferSelect;
+export type Profile = typeof profile.$inferSelect;
+export type Hero = typeof hero.$inferSelect;
+export type Project = typeof projects.$inferSelect;
