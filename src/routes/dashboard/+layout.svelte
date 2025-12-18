a839a6176fda8b7253b5f5103b878bb8907134a8
Roti18 <akuronif@gmail.com>
2025-12-18 21:32:48 +0700

    feat(dashboard): create main dashboard layout structure

---
+++ src/routes/dashboard/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Topbar from '$lib/components/Topbar.svelte';
+	import Sidebar from '$lib/components/Sidebar.svelte';
+	import ConfirmModal from '$lib/components/ConfirmModal.svelte';
+	import AlertModal from '$lib/components/AlertModal.svelte';
+</script>
+
+<div class="min-h-screen bg-black text-white">
+	<Topbar />
+
+	<div class="flex">
+		<Sidebar />
+
+		<main class="relative flex-1 p-6">
+			<slot />
+		</main>
+	</div>
+
+	<AlertModal />
+	<ConfirmModal />
+</div>
