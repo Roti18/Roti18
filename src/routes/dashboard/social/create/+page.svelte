64a394310e240c3345d184186f86cf0ddcf4e574
Roti18 <akuronif@gmail.com>
2025-12-20 03:47:58 +0700

    feat: Provide form for creating new social links with 'name' and 'url' inputs.

---
+++ src/routes/dashboard/social/create/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	import Input from '$lib/ui/Input.svelte';
+	import Card from '$lib/ui/Card.svelte';
+	import PageHeader from '$lib/ui/PageHeader.svelte';
+	import FormActions from '$lib/ui/FormActions.svelte';
+	import Button from '$lib/ui/Button.svelte';
+</script>
+
+<div class=" space-y-6">
+	<PageHeader back={() => goto('/dashboard/social')}>Create Social Link</PageHeader>
+
+	<Card>
+		<form method="POST" class="space-y-4">
+			<Input label="Name" name="name" required />
+			<Input label="URL" name="url" type="url" required />
+
+			<FormActions>
+				<Button type="submit">Create Social Link</Button>
+				<Button variant="ghost" on:click={() => goto('/dashboard/social')}>Cancel</Button>
+			</FormActions>
+		</form>
+	</Card>
+</div>
