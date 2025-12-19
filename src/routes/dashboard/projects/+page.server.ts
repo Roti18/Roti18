790c596dc622f0eaacd2d8a069a9c9a59bd7e262
Roti18 <akuronif@gmail.com>
2025-12-19 15:56:40 +0700

    feat(projects): Order projects by ID in descending order

--- src/routes/dashboard/projects/+page.server.ts
+++ src/routes/dashboard/projects/+page.server.ts
@@ -1,9 +1,11 @@
 import type { PageServerLoad } from './$types';
 import { db } from '$lib/server/db';
 import { projects } from '$lib/server/db/schema';
+import { desc } from 'drizzle-orm';
 
 export const load: PageServerLoad = async () => {
-	const allProjects = await db.select().from(projects);
+	const allProjects = await db.select().from(projects).orderBy(desc(projects.id));
+
 	return {
 		projects: allProjects
 	};
