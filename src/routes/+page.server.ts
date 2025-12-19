b6a2aafc60106a1ad8a4c0131b1cd8e9f8362358
Roti18 <akuronif@gmail.com>
2025-12-20 03:49:14 +0700

    feat: Load all social links for the main page frontend display.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,15 +1,17 @@
 import { db } from '$lib/server/db';
-import { hero, profile, projects } from '$lib/server/db/schema';
+import { hero, profile, projects, socialLinks } from '$lib/server/db/schema';
 import { desc } from 'drizzle-orm';
 
 export async function load() {
 	const [heroData] = await db.select().from(hero).limit(1);
 	const [profileData] = await db.select().from(profile).limit(1);
 	const projectData = await db.select().from(projects).orderBy(desc(projects.id));
+    const socialLinkData = await db.select().from(socialLinks);
 
 	return {
 		hero: heroData,
 		profile: profileData,
-		projects: projectData
+		projects: projectData,
+        socialLinks: socialLinkData
 	};
 }
