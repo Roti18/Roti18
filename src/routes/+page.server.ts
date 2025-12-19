af3bbc8ad869c72cc2f9aaa39e21347dca660ddf
Roti18 <akuronif@gmail.com>
2025-12-19 18:08:18 +0700

    feat: order projects by latest

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,11 @@
 import { db } from '$lib/server/db';
 import { hero, profile, projects } from '$lib/server/db/schema';
+import { desc } from 'drizzle-orm';
 
 export async function load() {
 	const [heroData] = await db.select().from(hero).limit(1);
 	const [profileData] = await db.select().from(profile).limit(1);
-	const projectData = await db.select().from(projects);
+	const projectData = await db.select().from(projects).orderBy(desc(projects.id));
 
 	return {
 		hero: heroData,
