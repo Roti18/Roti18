92b203352c24b674145c4413c814ab15f277cc47
Roti18 <akuronif@gmail.com>
2025-12-19 15:21:33 +0700

    fix: display avatars user (2)

--- src/lib/ui/Avatar.svelte
+++ src/lib/ui/Avatar.svelte
@@ -2,7 +2,8 @@
 	export let src: string | null = null;
 	export let name = '';
 
-	const imageSrc = src || 'avatars/default/default.webp';
+	const imageSrc =
+		src || 'https://zt2fm2rzmiug0mtb.public.blob.vercel-storage.com/avatars/default/default.webp';
 </script>
 
 <div class="flex h-10 w-full items-center justify-center overflow-hidden rounded-full bg-white/10">
