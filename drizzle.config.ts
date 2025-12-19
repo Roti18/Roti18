765a985439f2b3adc89b8e08b95efd4153600fdc
Roti18 <akuronif@gmail.com>
2025-12-19 10:37:50 +0700

    feat: Migrate Drizzle ORM configuration from SQLite to Turso

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,23 +1,30 @@
 // prod
-// import { defineConfig } from 'drizzle-kit';
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
 
-// if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!process.env.DATABASE_URL) {
+	throw new Error('DATABASE_URL is not set');
+}
+
+export default defineConfig({
+	schema: './src/lib/server/db/schema.ts',
+	out: './drizzle',
+	dialect: 'turso',
+	dbCredentials: {
+		url: process.env.DATABASE_URL,
+		authToken: process.env.DATABASE_AUTH_TOKEN
+	},
+	verbose: true,
+	strict: true
+});
+
+// local
+// import { defineConfig } from 'drizzle-kit';
 
 // export default defineConfig({
 // 	schema: './src/lib/server/db/schema.ts',
 // 	dialect: 'sqlite',
-// 	dbCredentials: { url: process.env.DATABASE_URL },
-// 	verbose: true,
-// 	strict: true
+// 	dbCredentials: {
+// 		url: process.env.DATABASE_URL!
+// 	}
 // });
-
-// local
-import { defineConfig } from 'drizzle-kit';
-
-export default defineConfig({
-	schema: './src/lib/server/db/schema.ts',
-	dialect: 'sqlite',
-	dbCredentials: {
-		url: process.env.DATABASE_URL!
-	}
-});
