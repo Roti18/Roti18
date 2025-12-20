529760a7bc76a099b48db1aeadf0b652b5720c30
Roti18 <akuronif@gmail.com>
2025-12-20 15:42:31 +0700

    feat: Add cursor-pointer to Avatar UI component

--- src/lib/ui/Avatar.svelte
+++ src/lib/ui/Avatar.svelte
@@ -6,6 +6,8 @@
 		src || 'https://zt2fm2rzmiug0mtb.public.blob.vercel-storage.com/avatars/default/default.webp';
 </script>
 
-<div class="flex h-10 w-full items-center justify-center overflow-hidden rounded-full bg-white/10">
+<div
+	class="flex h-10 w-full cursor-pointer items-center justify-center overflow-hidden rounded-full bg-white/10"
+>
 	<img src={imageSrc} alt={name} class="h-full w-full object-cover" />
 </div>
