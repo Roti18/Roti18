e8fe838d90971b43b7743c245baf557e29cf152c
Roti18 <akuronif@gmail.com>
2026-01-13 01:07:05 +0700

    feat(data): add central data barrel export

---
+++ data/index.ts
@@ -0,0 +1,21 @@
+// Central export for all data
+export { profile } from "./profile";
+export type { Profile } from "./profile";
+
+export { expertise } from "./expertise";
+export type { ExpertiseItem } from "./expertise";
+
+export { projects } from "./projects";
+export type { Project } from "./projects";
+
+export { stack } from "./stack";
+export type { StackItem } from "./stack";
+
+export { about } from "./about";
+export type { AboutContent, AboutSegment } from "./about";
+
+export { footer } from "./footer";
+export type { FooterData, SocialLink } from "./footer";
+
+export { navigation } from "./navigation";
+export type { NavLink } from "./navigation";
