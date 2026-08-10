5b8f3eb3dbc47b3b8a7e26cd8545e52c75192a61
Roti18 <akuronif@gmail.com>
2026-08-10 14:49:29 +0700

    perf: add lazy loading to project detail image

--- src/routes/(public)/project/[slug]/+page.svelte
+++ src/routes/(public)/project/[slug]/+page.svelte
@@ -48,7 +48,7 @@
 
 		{#if data.project.thumbnailUrl || data.project.originalUrl}
 			<div class="rounded-xl overflow-hidden border border-[#1f1f1f]">
-				<img src={data.project.originalUrl || data.project.thumbnailUrl} alt={data.project.title} class="w-full h-auto block" />
+				<img src={data.project.originalUrl || data.project.thumbnailUrl} alt={data.project.title} loading="lazy" class="w-full h-auto block" />
 			</div>
 		{/if}
 
